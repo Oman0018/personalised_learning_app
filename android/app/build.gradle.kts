a57f4ddd795cb51482ac4f74b17a571c752bd8d0
Oman0018 <oman0044@yahoo.co.uk>
2025-08-21 06:52:18 +0100

    android: pin compileSdk/targetSdk to 34 + buildTools 34.0.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,4 @@
+// android/app/build.gradle.kts
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -7,7 +8,10 @@ plugins {
 
 android {
     namespace = "com.example.personalised_learning_app"
-    compileSdk = flutter.compileSdkVersion
+
+    // Pin SDK versions to match CI workflow
+    compileSdk = 34
+    buildToolsVersion = "34.0.0"
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -22,10 +26,11 @@ android {
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.personalised_learning_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+
+        // Pin targetSdk to same as compileSdk
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 34
+
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
